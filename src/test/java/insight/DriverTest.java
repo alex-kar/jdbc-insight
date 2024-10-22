bc64de6ef2c4153e183f223b7a86c17cbccd33ff
Alex Karpov <karpov.aleksey.84@gmail.com>
2024-10-22 18:22:31 -0400

    test: DriverInsight#connect()

--- src/test/java/insight/DriverTest.java
+++ src/test/java/insight/DriverTest.java
@@ -1,10 +1,13 @@
 package insight;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 class DriverTest {
 
@@ -19,4 +22,11 @@ class DriverTest {
         Assertions.assertEquals(actual, expected);
     }
 
+    @Test
+    public void whenDetectWrongUrl_thenReturnNull_Test() throws SQLException {
+        Connection conn =
+                new DriverInsight().connect("jdbc:postgresql://127.0.0.1:5432/postgres", new Properties());
+
+        Assertions.assertNull(conn);
+    }
 }
